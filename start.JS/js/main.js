275e6571909d15c72bc5f1dd255ab48e6b8635ca
lschlotthauer <lucas6sic6@gmail.com>
2017-05-15 14:57:44 -0300

    Ejemplos niveles

--- start.JS/js/main.js
+++ start.JS/js/main.js
@@ -1,4 +1,5 @@
-var start = '{"favColor":"Orange", "FavSeason":"Fall"}';
+//Ejemplo 1
+/*var start = '{"favColor":"Orange", "FavSeason":"Fall"}';
 var myObject = JSON.parse(start);
 console.log(myObject);
 
@@ -6,4 +7,25 @@ var myString = JSON.stringify(myObject);
 console.log(myString);
 
 var myObject = JSON.parse(myString);
-console.log(myObject);
+console.log(myObject);*/
+
+//Ejemplo 2
+/*var theData1 = '{"first":"Paul", "last":"Cheney", "city":"Salem"}';
+var myObj = JSON.parse(theData1);
+console.log(myObj);
+
+document.getElementById('message').innerHTML = myObj.last;*/
+
+//Ejemplo 3
+/*var theData2 = '{"Jane":{"age":"29","degree":"MFA"}, "Jim":{"age":"49","degree":"MA"}}';
+var myObj = JSON.parse(theData2);
+console.log(myObj);
+
+document.getElementById('message').innerHTML = myObj.Jim.age;*/
+
+//Ejemplo 4
+/*var theData2 = '{"Jane":{"age":"29","degree":{"AAS":"VMI", "BA":"UVA"}}, "Jim":{"age":"49","degree":"MA"}}';
+var myObj = JSON.parse(theData2);
+console.log(myObj);
+
+document.getElementById('message').innerHTML = myObj.Jane.degree.BA;*/
